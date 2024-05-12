ce2ebd16f503858d0f36769131cd96afc46ebeb5
David <darevalo-2022062@kinal.edu.gt>
2024-05-12 09:05:38 -0600

    Inicio del protyecto

---
+++ blogCool/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { useRoutes } from 'react-router-dom'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
